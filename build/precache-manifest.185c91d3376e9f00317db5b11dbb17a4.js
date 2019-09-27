64119c45416e9593b850d36784dd4b3a932d988b
lihaoxian <531386485@qq.com>
2019-09-27 10:25:32 +0800

    build

---
+++ build/precache-manifest.185c91d3376e9f00317db5b11dbb17a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329e070e047585501c526a406a9c489e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55131d632b0b5b2acb56",
+    "url": "/static/css/2.41a373cd.chunk.css"
+  },
+  {
+    "revision": "136533b5e43d991157c8",
+    "url": "/static/css/main.4378b537.chunk.css"
+  },
+  {
+    "revision": "55131d632b0b5b2acb56",
+    "url": "/static/js/2.9089f38f.chunk.js"
+  },
+  {
+    "revision": "136533b5e43d991157c8",
+    "url": "/static/js/main.9df1f887.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
